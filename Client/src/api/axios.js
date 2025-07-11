1e44710b98598bf6547e826395f0634bd2fc7f63
Abhishek <144004785+abhikc2811@users.noreply.github.com>
2025-07-11 21:22:51 +0530

    Update axios.js

--- Client/src/api/axios.js
+++ Client/src/api/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 export const axiosInstance = axios.create({
-  baseURL: 'http://localhost:5000/api',
+  baseURL: 'https://student-progress-production.up.railway.app/api',
   withCredentials: true,
 });
