5c13806b3c8003e02bc27cfa5da822ef8df5eedd
Abhishek <abhikc2811@gmail.com>
2025-07-22 20:17:09 +0530

    update cron logic

--- server/src/server.js
+++ server/src/server.js
@@ -8,14 +8,15 @@ import authRoutes from './routes/auth.routes.js';
 import studentRoutes from './routes/student.routes.js';
 import datasyncRoutes from './routes/datasync.route.js';
 import adminRoutes from './routes/admin.routes.js'
+import { runCodeforcesSync } from './cron/syncCodeforces.js';
+import { inactivityReminders } from './cron/inactivityReminder.js';
+import Setting from './models/setting.model.js';
 
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT;
-
-const schedule = process.env.SYNC_CRON_SCHEDULE || '0 2 * * *';
 
 app.use(cors({
     origin: 'https://student-progress-management-system-murex.vercel.app',
@@ -30,19 +31,35 @@ app.use('/api/students', studentRoutes);
 app.use('/api/', datasyncRoutes);
 app.use('/api/admin/', adminRoutes);
 
-cron.schedule(schedule, () => {
-  console.log(`🔄 Running scheduled Codeforces sync at: ${schedule}`);
-  runCodeforcesSync().catch(err =>
-    console.error('Scheduled sync error:', err)
-  );
-});
+const startServer = async () => {
+  try {
+    await connectDB();
+    const settings = await Setting.findOne();
+    const schedule = settings?.cron || '0 2 * * *'; 
+    const inactivityTime = settings?.inactivityDays || 7;
 
-cron.schedule('0 9 * * *', () => {
-  console.log('🔔 Running inactivity detection');
-  inactivityReminders();
-});
+    console.log(`✅ Using cron schedule: ${schedule}`);
+    console.log(`📨 Inactivity reminders will trigger after ${inactivityTime} days`);
 
-app.listen(PORT, () => {
-    console.log(`Server started on port ${PORT}`);
-    connectDB();
-});
+    cron.schedule(schedule, () => {
+      console.log(`🔄 Running scheduled Codeforces sync at: ${new Date().toLocaleString()}`);
+      runCodeforcesSync().catch(err =>
+        console.error('Scheduled sync error:', err)
+      );
+    });
+
+    cron.schedule('0 9 * * *', () => {
+      console.log(`🔔 Running inactivity detection at: ${new Date().toLocaleString()}`);
+      inactivityReminders();
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server started on port ${PORT}`);
+    });
+  } catch (err) {
+    console.error('❌ Failed to start server:', err);
+    process.exit(1);
+  }
+};
+
+startServer();
